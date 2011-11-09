3ce1c0f37d3f1386eb214a53a784b1cd30588586
Alex V <simplex0@gmail.com>
2011-11-09 09:47:36 -0500

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inertia::Application.config.secret_token = '38dcd5c64512128660a78e969b0467be4d916905b5f85135344930a67e5e693d5075e581267f1d7c474ecb2ba9e986da857dfc52c22d08bbba27ade1ffc91e3c'
