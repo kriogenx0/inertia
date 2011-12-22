7a619d9f155ba08e4ce93b284c5c65bab9103583
Alex V <simplex0@gmail.com>
2011-12-22 17:48:29 -0500

    Config update

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,13 @@ end
 
 module Inertia
   class Application < Rails::Application
+    config.generators do |g|
+      # g.controller          :helper => false, :test_framework => false
+      g.template_engine     :haml
+      g.test_framework      :rspec, :fixture => true, :views => false, :controllers => false
+      # g.integration_tool    :rspec, :fixture => true, :views => false
+      # g.fixture_replacement :fabrication
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
